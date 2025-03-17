11e29df40b2f4040c323812c7ebc8e25f3d99378
efemen <senef19@itu.edu.tr>
2025-03-17 17:25:08 +0000

    nice stuff

--- Manual_WT_Control.py
+++ Manual_WT_Control.py
@@ -1,8 +1,12 @@
 from acquisition_tools import *
 
 WT = wind_tunnel()
-U_inf = 0 # m/s
+U_inf = 8 # m/s
 
-WT.set_U0(U_inf)
+# WT.set_U0(U_inf)
+# WT.set_fan_speed(12.7)
+# WT.set_fan_speed(17)
+# WT.set_fan_speed(8.55)
+WT.set_fan_speed(0)
 
 WT.close()
